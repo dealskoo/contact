fa5de614861e27e6f17e53416f3b1373bd9ad224
jabber <2898117012@qq.com>
2022-02-13 19:00:52 +0800

    fix: web middleware

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Dealskoo\Contact\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['throttle:6,1'])->post('/contact', [ContactController::class, 'handle'])->name('contact.inbox');
+Route::middleware(['web', 'throttle:6,1'])->post('/contact', [ContactController::class, 'handle'])->name('contact.inbox');

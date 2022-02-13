97ab076537410caf22a4e93b05a1e4567bec9fdd
jabber <2898117012@qq.com>
2022-02-13 19:02:53 +0800

    fix: web middleware

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Dealskoo\Contact\Tests;
 
 use Dealskoo\Contact\Providers\ContactServiceProvider;
+use Illuminate\Encryption\Encrypter;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -20,6 +21,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
+        $app['config']->set('app.key', 'base64:' . base64_encode(Encrypter::generateKey('AES-256-CBC')));
         $app['config']->set('mail.reply_to.address', 'hello@dealskoo.com');
         $app['config']->set('database.default', 'testing');
         $app['config']->set('database.connections.testing', [
